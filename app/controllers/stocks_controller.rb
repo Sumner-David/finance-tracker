110da7b40414621e6566bfe9f55272be06b12563
David <davidphilipsumner@gmail.com>
2021-02-14 13:51:47 +0000

    Add JS rendering for portfolio, create stocks_User table & relationship

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,16 +5,22 @@ class StocksController < ApplicationController
       @stock = Stock.new_lookup(params[:stock])
       
       if @stock 
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result'}
+        end
       else 
-        flash[:alert] = "Please enter a valid Ticker"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid Ticker"
+          format.js { render partial: 'users/result' }
+        end
+        
       end
 
     else 
-      flash[:alert] = "No Ticker Entered"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a symbol to search"
+          format.js { render partial: 'users/result' }
+        end
     end
-      
   end
 end
