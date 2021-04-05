ec7bf104fc310460515431a508720a6b8f2ec704
David <davidphilipsumner@gmail.com>
2021-04-05 17:29:01 +0100

    Add a my_friends page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,20 +3,19 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      
-      if @stock 
+
+      if @stock
         respond_to do |format|
           format.js { render partial: 'users/result'}
         end
-      else 
+      else
         respond_to do |format|
           flash.now[:alert] = "Please enter a valid Ticker"
           format.js { render partial: 'users/result' }
         end
-        
       end
 
-    else 
+    else
       respond_to do |format|
           flash.now[:alert] = "Please enter a symbol to search"
           format.js { render partial: 'users/result' }
