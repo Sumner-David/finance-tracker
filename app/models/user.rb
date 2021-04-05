30f4959dbb5fa663f0dd1a7c18489c8ef2d9de88
David <davidphilipsumner@gmail.com>
2021-04-05 17:10:15 +0100

    Create friendships Model using self referencing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-  
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -24,7 +26,7 @@ class User < ApplicationRecord
   end
 
 
-  def full_name 
+  def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
   end
